1fde6980414651a2a9bf1b0bc58f193087d6f2ff
JonJonSource <Jon.Ste@outlook.com>
2025-02-07 15:32:37 +0100

    slack message on build fail

--- src/test/kotlin/be/kunlabora/crafters/kunlaquota/KunlaquotaApplicationTests.kt
+++ src/test/kotlin/be/kunlabora/crafters/kunlaquota/KunlaquotaApplicationTests.kt
@@ -2,12 +2,14 @@ package be.kunlabora.crafters.kunlaquota
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import kotlin.test.assertFalse
 
 @SpringBootTest(classes = [TestKunlaquotaApplication::class])
 class KunlaquotaApplicationTests {
 
 	@Test
 	fun contextLoads() {
+		assertFalse(true)
 	}
 
 }
